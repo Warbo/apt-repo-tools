91aa20ee91853f82933c5f04dc86b527aa59cb1d
Chris Warburton <chris@chris-portable.(none)>
2009-05-31 23:55:53 +0000

    Added make_packages.py

---
+++ make_packages.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+import time
+
+def get_id(lines):
+	id = ''
+	to_return = ''
+	for line in lines:
+		if line.startswith("deb ") or line.startswith('deb-src '):
+			for bit in line.split(' ', 1)[1].split(' '):
+				x = bit.lower().strip().replace("http://", "")
+				x = x.replace("/","-").replace(".","-").replace(":","-")
+				id = (id + x + "-").lower()
+			for char in id:
+				if char.isalnum() and (not char == '~'):
+					to_return = to_return + char
+				else:
+					to_return = to_return + '-'
+			while to_return.startswith('-'):
+				to_return = to_return[1:]
+			while to_return.endswith('-'):
+				to_return = to_return[:-1]
+			return to_return
+
+def makeControl(name):
+	# Package details are kept in a structured text file called 'control'
+	# First we make some folders to work in
+	try:
+		os.makedirs('temp/package/DEBIAN')
+	except OSError:
+		pass
+	controlFile = open('temp/package/DEBIAN/control', 'w')
+	# Then we add relevant information
+	controlFile.write('Package: ' + name + '\n')
+	controlFile.write('Priority: extra\n')
+	controlFile.write('Section: repositories\n')
+	controlFile.write('Installed-Size: 1\n')		# FIXME
+	controlFile.write('Maintainer: chriswarbo@gmail.com\n')
+	controlFile.write('Architecture: all\n')
+	controlFile.write('Version: '+time.strftime("%Y%m%d", time.gmtime())+'\n')
+	#dependsLine = 'Depends: '
+	#for current in self.metaPackage.getDepends():
+	#	dependsLine = dependsLine + current + ', '
+	#dependsLine = dependsLine[:-2] + '\n'
+	#controlFile.write(dependsLine)
+	#controlFile.write('Conflicts: ' + self.metaPackage.getName() + '-setup\n')		# By conflicting with the setup package it will get removed by APT
+	controlFile.write('Description: This package provides the ' + name + ' repository.\n')
+	controlFile.close()
+
+def makeDebianFile():
+	# This is required by dpkg
+	debianFile = open('temp/package/DEBIAN/debian-binary', 'w')
+	debianFile.write('2.0')
+	debianFile.close()
+
+def buildPackage(filename):
+	# Builds the package
+	try:
+		shutil.rmtree('temp/package/etc/apt/sources.list.d')
+		os.makedirs('temp/package/etc/apt/sources.list.d')
+	except OSError:
+		pass
+	print filename
+	shutil.copy('temp/lists/'+filename, 'temp/package/etc/apt/sources.list.d')
+	os.popen('dpkg-deb -b ' + 'temp/package temp/'+filename+'.deb', 'r')		# This builds the package
+	try:
+		os.wait()
+	except OSError:
+		pass
+
+	#os.popen('cd ' + os.getcwd() + '/temp/ + '/' + self.metaPackage.getName() + '/data && reprepro includedeb ' + self.codename + ' ' + self.location + '/' + self.metaPackage.getName() + '/' + self.metaPackage.getName() + '.deb', 'r')		# This inserts it into the repository
+	#	try:
+	#		os.wait()
+	#	except:
+	#		pass
+	#	os.popen('rm ' + self.location + '/' + self.metaPackage.getName() + '/' + self.metaPackage.getName() + '.deb', 'r')		# This removes the original file now that a copy is in the repository
+	#	try:
+	#		os.wait()
+	#	except:
+	#		pass
+
+for filename in os.listdir('temp/lists'):
+	f = open('temp/lists/'+filename, 'r')
+	makeControl(get_id(f.readlines()))
+	makeDebianFile()
+	buildPackage(filename)
+	f.close()
