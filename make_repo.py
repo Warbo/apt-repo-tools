53bd92505688972eb4e3e9c731d48778a645ecb8
Chris Warburton <ChrisWarbo@gmail.com>
2010-09-25 14:10:15 +0000

    make_repo.py now builds a repository containing all of the generated packages.
    Next job is to get it segregating packages based on their release

--- make_repo.py
+++ make_repo.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+"""This will build a Debian repository in a folder called 'data' and insert any
+packages it finds in a folder called 'packages'"""
 
-### PAY NO ATTENTION TO THIS YET! I COPY-PASTA'D IT FROM ANOTHER TOOL!
+import os
 
 def makeRepoControlFile():
 	'''This makes the configuration file for the repository which is
@@ -10,7 +12,7 @@ def makeRepoControlFile():
 	# FIXME: Other architectures and distribution should be added if specified
 	# as includes
 	originLine = 'Origin: Repository generator'		# FIXME: How to get a meaningful name when run under gksudo?
-	labelLine = 'Label: ' + self.metaPackage.getName()
+	labelLine = 'Label: repository list'
 	# The 'codename' is the release name, for example Debian 4.0 is "etch",
 	# Ubuntu 7.04 is "feisty".
 	# Here we only grab the current system's, although this should be expanded
@@ -18,15 +20,15 @@ def makeRepoControlFile():
 	# repositories (compression should sort out space issues of redundant data)
 	codenameOut = os.popen('lsb_release -c', 'r')
 	for line in codenameOut:
-		self.codename = line
-		self.codename = self.codename[10:]
-		if self.codename.endswith('\n'):
-			self.codename = self.codename[:-1]
-		while self.codename.startswith(' '):
-			self.codename = self.codename[1:]
-		while self.codename.endswith(' '):
-			self.codename = self.codename[:-1]
-	codenameLine = 'Codename: ' + self.codename
+		codename = line
+		codename = codename[10:]
+		if codename.endswith('\n'):
+			codename = codename[:-1]
+		while codename.startswith(' '):
+			codename = codename[1:]
+		while codename.endswith(' '):
+			codename = codename[:-1]
+	codenameLine = 'Codename: ' + codename
 	# The next bit finds the version of the current distribution
 	versionOut = os.popen('lsb_release -r', 'r')
 	for line in versionOut:
@@ -43,15 +45,30 @@ def makeRepoControlFile():
 	arch = os.popen('uname -m', 'r')
 	for line in arch:
 		if '64' in line:
-			self.architecture = 'amd64'
+			architecture = 'amd64'
 		elif ('i' in line) and ('86' in line):
-			self.architecture = 'i386'
+			architecture = 'i386'
 		elif 'ppc' in line:
-			self.architecture = 'ppc'
-	architecturesLine = 'Architectures: ' + self.architecture
+			architecture = 'ppc'
+	architecturesLine = 'Architectures: ' + architecture
 	# FIXME: Add some more architectures here
+
 	componentsLine = 'Components: main'		# FIXME: Is it worth specifying these according to included packages?
-	descriptionLine = 'Description: This package repository contains packages for the ' + self.metaPackage.getName() + ' service pack.'
+	descriptionLine = 'Description: This package repository contains packages which enable Debian repositories'
+
 	# Now the data is written
-	self.distribConf.write(originLine + '\n' + labelLine + '\n' + codenameLine + '\n' + versionLine + '\n' + architecturesLine + '\n' + componentsLine + '\n' + descriptionLine + '\n')
-	self.distribConf.close()
+	if 'data' in os.listdir(os.getcwd()) and 'conf' in os.listdir(os.getcwd()+'/data'):
+		pass
+	else:
+		os.makedirs(os.getcwd() + '/data/conf')		# This folder is needed by the reprepro tool
+	distribConf = open(os.getcwd() + '/data/conf/distributions', 'w')
+	distribConf.write(originLine + '\n' + labelLine + '\n' + codenameLine + '\n' + versionLine + '\n' + architecturesLine + '\n' + componentsLine + '\n' + descriptionLine + '\n')
+	distribConf.close()
+
+	# Now build the repository
+	for current in [os.getcwd()+'/packages/'+p for p in os.listdir(os.getcwd() + '/packages') if p.endswith('.deb')]:
+		os.popen('cd ' + os.getcwd() + '/data && reprepro --ignore=extension includedeb ' + codename + ' ' + current, 'r')
+
+# Run the above if we've been called as a program
+if __name__ == '__main__':
+	makeRepoControlFile()
